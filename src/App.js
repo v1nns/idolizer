08df44340cc55f686750344a4f95b9d10b822070
vinicius.longaray <vinicius.longaray@datacom.ind.br>
2020-02-10 16:09:06 -0300

    First commit containing the project structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import smile from './images/smile.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={smile} className="App-logo" alt="logo" />
+        <p>
+          Zézinho
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
